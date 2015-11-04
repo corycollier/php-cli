0ff55b461b06970a871193d622b55bb1a6254577
Cory Collier <corycollier@corycollier.com>
2015-11-03 23:07:33 -0500

    closing out for the night. I'm tired and these tests are starting to look stupid.

--- src/Parser.php
+++ src/Parser.php
@@ -6,10 +6,64 @@ class Parser
 {
     protected $argv = array();
     protected $argc = 0;
+    protected $parsedArgs = array();
 
     public function __construct(array $argv = array(), $argc = 0)
     {
         $this->argv = $argv;
         $this->argc = $argc;
+
+        $this->init();
+    }
+
+    /**
+     * Local implementation of the init hook.
+     * @return PhpCli\Parser Returns $this, for object-chaining.
+     */
+    public function init()
+    {
+        $this->parseArgv($this->argv);
+        return $this;
+    }
+
+    /**
+     * Parses an array of arguments.
+     *
+     * @param  array $argv The raw array of arguments
+     *
+     * @return PhpCli\Parser Returns $this, for object-chaining.
+     */
+    protected function parseArgv($argv)
+    {
+        foreach ($this->argv as $arg) {
+            $parts = explode('=', $arg);
+            if (! isset($parts[1])) {
+                continue;
+            }
+            $value = $parts[1];
+            $key   = strtr($parts[0], array(
+                '-' => ''
+            ));
+
+            $this->parsedArgs[$key] = $value;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get the parsed arguments.
+     *
+     * @return array The parsed arguments.
+     */
+    public function getArgs()
+    {
+        return $this->parsedArgs;
+    }
+
+
+    public function getArg ($name = '')
+    {
+
     }
 }
--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use PhpCli\Parser;
+
 class ParserTest extends PHPUnit_Framework_TestCase
 {
   /**
@@ -16,5 +18,87 @@ class ParserTest extends PHPUnit_Framework_TestCase
     $argc = count($argv);
 
     $sut = new PhpCli\Parser($argv, $argc);
+
+    $reflection = new ReflectionClass('PhpCli\\Parser');
+
+    $reflection_property = $reflection->getProperty('argv');
+    $reflection_property->setAccessible(true);
+    $this->assertEquals($argv, $reflection_property->getValue($sut));
+
+    $reflection_property = $reflection->getProperty('argc');
+    $reflection_property->setAccessible(true);
+    $this->assertEquals($argc, $reflection_property->getValue($sut));
+  }
+
+  public function testInit()
+  {
+    $argv = array(
+      'path/to/file',
+      'asdf=asdf',
+      '--qwer=qwer'
+    );
+
+    $sut = $this->getMockBuilder('PhpCli\Parser')
+      ->disableOriginalConstructor()
+      ->setMethods(array('parseArgv'))
+      ->getMock();
+
+    $reflection = new ReflectionClass('PhpCli\\Parser');
+
+    $reflection_property = $reflection->getProperty('argv');
+    $reflection_property->setAccessible(true);
+    $reflection_property->setValue($sut, $argv);
+
+    $sut->expects($this->once())
+      ->method('parseArgv')
+      ->with($this->equalTo($argv));
+
+    $result = $sut->init();
+    $this->assertEquals($sut, $result);
+  }
+
+  /**
+   * Tests the parseArgv method.
+   *
+   * @param array $expected What the pasred Argv values should be.
+   * @param array $argv The raw argv values.
+   *
+   * @dataProvider dataParseArgv
+   */
+  public function testParseArgv($expected, $argv)
+  {
+    $sut = new PhpCli\Parser($argv);
+    $reflection = new ReflectionClass('PhpCli\\Parser');
+
+    $reflection_method = $reflection->getMethod('parseArgv');
+    $reflection_method->setAccessible(true);
+    $result = $reflection_method->invoke($sut, $argv);
+
+    $this->assertEquals($sut, $result);
+
+    $reflection_property = $reflection->getProperty('argv');
+    $reflection_property->setAccessible(true);
+    $this->assertEquals($expected, $reflection_property->getValue($sut));
+  }
+
+  public function dataParseArgv()
+  {
+    return array(
+
+      'empty argv, empty expectations' => array(
+        'expected' => array(),
+        'argv' => array(),
+      ),
+
+      'simple argv, simple expectations' => array(
+        'expected' => array(
+          'key' => 'value'
+        ),
+        'argv' => array(
+          'key=value',
+        ),
+      ),
+
+    );
   }
 }
