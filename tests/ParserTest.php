482e587ea350db1d8fcaa6a22b7ef61e82110613
Cory Collier <corycollier@corycollier.com>
2015-11-03 22:02:14 -0500

    updating the unit tests

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -2,8 +2,19 @@
 
 class ParserTest extends PHPUnit_Framework_TestCase
 {
+  /**
+   * Tests the parser's ability to set things on the constructor
+   */
   public function testConstructor()
   {
+    $argv = array(
+      'path/to/file',
+      'asdf=asdf',
+      '--qwer=qwer'
+    );
 
+    $argc = count($argv);
+
+    $sut = new PhpCli\Parser($argv, $argc);
   }
 }
